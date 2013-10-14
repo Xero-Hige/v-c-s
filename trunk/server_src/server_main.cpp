73c3b5328879e6a812c52d4216481241703c7c93
juan6691@gmail.com <juan6691@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-10-14 22:31:40 +0000

    Subo la 'adaptación' del tp4. Seguramente hay una manera mas copada de tener diferentes proyectos en el mismo repo y que no te tire que tiene definiciones multiples del main.

---
+++ trunk/server_src/server_main.cpp
@@ -0,0 +1,69 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 29/09/2013
+ *      Author: juan
+ */
+
+
+#include <iostream>
+#include "server_Server.h"
+#include "../common_src/common_Thread.h"
+#include <pthread.h>
+#include <string>
+#include <vector>
+
+#define CHAR_DE_SALIDA 'q'
+#define SEPARADOR_PORTS ','
+
+namespace std{
+
+/*
+ * charDeSalidaApretado devuelve true si CHAR_DE_SALIDA fue presionado.
+ */
+bool charDeSalidaApretado(){
+	if (cin.get() == CHAR_DE_SALIDA) return true; //.get es bloqueante...
+	return false;
+}
+
+/*
+ * getListaPuertos devuelve una lista de ints que corresponden a los puertos
+ * donde el server tiene que escuchar.
+ */
+vector<int*> * getListaPuertos(int argc, char * argv[]){
+	vector<int*> * l_ports = new vector<int*>();
+	if (argc != 2) return NULL;
+	string str;
+	int i = 0;
+	char c = (argv[1])[i];
+	while (c != '\0'){
+		if (c != SEPARADOR_PORTS) {
+			str += c;
+		} else {
+			l_ports->push_back(new int(atoi(str.c_str())));
+			str.clear();
+		}
+		i++;
+		c = (argv[1])[i];
+	}
+	//Agrego el ultimo
+	if (str.length() > 0) l_ports->push_back(new int (atoi(str.c_str())));
+	return l_ports;
+}
+
+
+}
+
+int main(int argc, char *argv[]){
+	std::vector<int*> * list_puertos = std::getListaPuertos(argc, argv);
+	if (!list_puertos) return 0;
+	std::Server s();
+//	s.serverListen(list_puertos);
+//	s.acceptConnections();
+	while (!std::charDeSalidaApretado()){ //ignorar
+	}
+//	s.dejarDeAceptarConex();
+	if (list_puertos) delete list_puertos;
+	return 0;
+}
+
