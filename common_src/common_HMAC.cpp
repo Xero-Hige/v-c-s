f739b92b348b2cde22aaeae024d61a8bdd69bdfd
juan6691@gmail.com <juan6691@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-10-16 01:46:30 +0000

    HMAC andando

---
+++ common_src/common_HMAC.cpp
@@ -0,0 +1,60 @@
+/*
+ * HMAC.cpp
+ *
+ *  Created on: 15/10/2013
+ *      Author: juan
+ */
+
+
+//sudo apt-get install libssl-dev
+//-lssl -lcrypto
+#include "common_HMAC.h"
+#include <openssl/sha.h> //SHA1
+#include <string>
+#include <sstream>
+
+// unsigned char *SHA1(const unsigned char *d, unsigned long n,
+//                 unsigned char *md);
+
+#define SHA_DIGEST_SIZE 20 //160bits
+#define IPAD 0x36
+#define OPAD 0x5c
+
+void mySHA1(const std::string msg, unsigned char * digest){
+//	digest.clear();
+//	digest.resize(SHA_DIGEST_SIZE);
+//	unsigned char c_digest[SHA_DIGEST_SIZE];
+	//Hago el casteo tranquilo porque ningun ASCII es negativo
+	SHA1((unsigned char *)msg.c_str(), msg.size(), digest);
+}
+
+void stringXor (std::string & s, const char key){
+	char * c_str = (char *)s.c_str();
+	for (unsigned i = 0; i < s.size(); i++){
+		//Hace el xor a cada byte
+		*( c_str + i) = (*(c_str + i)) ^ key;
+	}
+}
+
+void HMAC(std::string key, std::string msg, unsigned char * digest){
+	std::stringstream ss;
+	//Prepara la inner key
+	std::string i_key = key;
+	stringXor(i_key, IPAD);
+	ss << i_key;
+	ss << msg;
+	//hashea inner key + msg
+	unsigned char SHA1_digest[SHA_DIGEST_SIZE];
+	mySHA1(ss.str(), SHA1_digest);
+	//borra el stream
+	ss.str(std::string());
+	ss.clear();
+	//prepara outer key
+	std::string o_key = key;
+	stringXor(o_key, OPAD);
+	ss << o_key;
+	ss.write((char *)SHA1_digest, SHA_DIGEST_SIZE);
+	//hashea outer key + msg
+	mySHA1(ss.str(), digest);
+}
+
