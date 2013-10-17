2ebf98f4fc0d92ff31e72e966c1b824a4fe12221
juan6691@gmail.com <juan6691@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-10-17 21:25:31 +0000

    Nuevas clases... estoy probando algo. Alguien podria mover las carpetas _src a la sandbox? tengo problemas para hacerlo :(

---
+++ trunk/server_src/serverRoom.cpp
@@ -0,0 +1,18 @@
+/*
+ * serverRoom.cpp
+ *
+ *  Created on: 17/10/2013
+ *      Author: juan
+ */
+
+#include "serverRoom.h"
+
+server_Room::server_Room() {
+	// TODO Auto-generated constructor stub
+
+}
+
+server_Room::~server_Room() {
+	// TODO Auto-generated destructor stub
+}
+
