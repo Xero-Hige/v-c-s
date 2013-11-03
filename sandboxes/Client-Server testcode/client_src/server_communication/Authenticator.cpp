c8ef2b21281ed6aa7ac510218e3c8b6a2b301a92
juan6691@gmail.com <juan6691@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-11-03 23:44:55 +0000

    d

---
+++ sandboxes/Client-Server testcode/client_src/server_communication/Authenticator.cpp
@@ -0,0 +1,46 @@
+/*
+ * Authenticator.cpp
+ *
+ *  Created on: 21/10/2013
+ *      Author: juan
+ */
+
+#include "Authenticator.h"
+#include "../../libs/msgs/MsgConstants.h"
+#include "../../libs/communication_protocol/BigEndianProtocol.h"
+#include <cstring>
+
+namespace std {
+
+Authenticator::Authenticator(Client * c) {
+	this->client = c;
+}
+
+bool Authenticator::receiveAuthVerif(){
+	string s(IDS_VERIF);
+	return verificateMessage(s);
+}
+
+bool Authenticator::verificateMessage(string msg){
+	//Compara si el mensaje que envio el server coincide con 'msg'
+	string recvd_msg;
+	client->sock->recvMsg(recvd_msg);
+	if (recvd_msg.compare(msg) == 0) {
+		return true;
+	}
+	return false;
+}
+
+bool Authenticator::sendIds
+(string & user, string & passwd, string & auth_type){
+	this->client->sock->sendMsg(auth_type);
+	this->client->sock->sendMsg(user);
+	this->client->sock->sendMsg(passwd);
+	if (this->receiveAuthVerif()) return true;
+	return false;
+}
+
+Authenticator::~Authenticator() {
+}
+
+} /* namespace std */
---
+++ sandboxes/Client-Server testcode/client_src/server_communication/Authenticator.h
@@ -0,0 +1,27 @@
+/*
+ * Authenticator.h
+ *
+ *  Created on: 21/10/2013
+ *      Author: juan
+ */
+
+#ifndef AUTHENTICATOR_H_
+#define AUTHENTICATOR_H_
+
+#include "../Client.h"
+
+namespace std {
+
+class Authenticator {
+	Client * client;
+	bool verificateMessage(string msg);
+	bool receiveAuthVerif();
+
+public:
+	Authenticator(Client * c);
+	bool sendIds(string & user, string & passwd, string & auth_type);
+	virtual ~Authenticator();
+};
+
+} /* namespace std */
+#endif /* AUTHENTICATOR_H_ */
