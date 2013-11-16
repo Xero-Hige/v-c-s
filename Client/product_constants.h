3272c5aefac398d3167bc1eb24bdec8ff125e355
bruno290@gmail.com <bruno290@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-11-16 22:55:36 +0000

    Constantes de los productos para usar en la lógica y la interfaz

---
+++ Client/product_constants.h
@@ -0,0 +1,43 @@
+/*
+ * product_constants.h
+ *
+ * Created on: Nov 16, 2013
+ * 
+ * Copyright 2013 Bruno Merlo Schurmann <brunomerloschurmann@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses 
+ */
+
+#ifndef PRODUCT_CONSTANTS_H_
+#define PRODUCT_CONSTANTS_H_
+
+/* Constants of the product's colors and types */
+
+// Color constants
+#define NO_COLOR -1
+#define UNCOLORED 0
+#define RED      1
+#define YELLOW   2
+#define GREEN    3
+#define BLUE     4
+#define VIOLET   5
+
+// Type constants
+#define NO_TYPE -1
+#define BUTTON 0
+#define V_BAR 1
+#define H_BAR 2
+#define STAR 3
+
+#endif /* PRODUCT_CONSTANTS_H_ */
