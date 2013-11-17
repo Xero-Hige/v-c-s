603e25a9a7cc39e95952433757cbd51588c6940e
juan6691@gmail.com <juan6691@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-11-17 20:17:34 +0000

    update de este wrapper

--- trunk/libs/wrappers/Database.cpp
+++ trunk/libs/wrappers/Database.cpp
@@ -15,10 +15,19 @@ int Database::open(std::string path){
 	return sqlite3_open(path.c_str(), &db);
 }
 
-int Database::exec(std::string query, std::string errmsg){
-	//No hay ningun dato sobre un error ya que estoy pasando todo NULL
-	//para simplificar.
-	return sqlite3_exec(db, query.c_str(), NULL, NULL, NULL);
+int Database::prepare(std::string query, sqlite3_stmt * stmt){
+	return sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+}
+
+int Database::query(std::string query){
+	sqlite3_stmt * new_stmt;
+	prepare(query.c_str(), new_stmt);
+	step(new_stmt);
+	sqlite3_finalize(new_stmt);
+}
+
+int Database::step(sqlite3_stmt * stmt){
+	return sqlite3_step(stmt);
 }
 
 int Database::close(){
