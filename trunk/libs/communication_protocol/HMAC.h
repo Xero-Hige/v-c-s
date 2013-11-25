ee232f1ea37c2d7e4836c655df9d67a2755e8206
juan6691@gmail.com <juan6691@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-11-25 17:01:25 +0000

    HMAC listo para ser integrado..

--- trunk/libs/communication_protocol/HMAC.h
+++ trunk/libs/communication_protocol/HMAC.h
@@ -10,10 +10,22 @@
 
 #include <string>
 
+#define SHA_DIGEST_SIZE 20 //160bits
+
 /*
- * HMAC devuelve en digest los 20 bytes resultantes de aplicarle la firma
- * a 'msg' con la clave 'key'.
+ * HMAC devuelve en digest los SHA_DIGEST_SIZE bytes resultantes de aplicarle
+ * la firma a 'msg' con la clave 'key'.
  */
 void HMAC(std::string key, std::string msg, unsigned char * digest);
+
+/*
+ * HMACSignedMsg hace lo mismo que HMAC pero lo apendea al msg.
+ */
+void HMACSignedMsg(std::string & msg, std::string key);
+
+/*
+ * checkSignedMsg verifica si el hash digest al final de 'signed_msg' es correcto.
+ */
+bool checkSignedMsg(std::string signed_msg, std::string key);
 
 #endif /* COMMON_HMAC_H_ */
