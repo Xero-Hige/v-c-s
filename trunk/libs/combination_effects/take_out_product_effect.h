8762abcb9f8e23d0a4da1f0dc1f202fae6f99071
bruno290@gmail.com <bruno290@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-11-26 06:17:37 +0000

    No se commiteo TakeOutProductEffect en el anterior.

---
+++ trunk/libs/combination_effects/take_out_product_effect.h
@@ -0,0 +1,38 @@
+/*
+ * take_out_product_effect.h
+ *
+ * Created on: Nov 26, 2013
+ * 
+ * Copyright 2013 Bruno Merlo Schurmann <brunomerloschurmann@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses 
+ */
+
+#ifndef TAKE_OUT_PRODUCT_EFFECT_H_
+#define TAKE_OUT_PRODUCT_EFFECT_H_
+
+#include "combination_effect.h"
+
+#include "../position/position.h"
+
+/* Clase que representa la eliminación de un producto simple */
+class TakeOutProductEffect : public CombinationEffect {
+public:
+    TakeOutProductEffect(Position origin)
+        : CombinationEffect(origin) {}
+    void applyEffect() {}
+    ~TakeOutProductEffect() {}
+};
+
+#endif /* TAKE_OUT_PRODUCT_EFFECT_H_ */
