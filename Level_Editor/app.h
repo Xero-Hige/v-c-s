a74be94c9be5c23288f7502e616860e9519632af
Gaston Martinez <Xero-Hige@users.noreply.github.com>
2013-11-27 00:33:20 +0000

    Agregado para arrancar con el level editor

---
+++ Level_Editor/app.h
@@ -0,0 +1,78 @@
+/**
+ app.h
+
+ Copyright 2013 Gaston Martinez Gaston.martinez.90@gmail.com
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+#ifndef APP_H_
+#define APP_H_
+
+#define STATUS_RUNNING 0
+#define STATUS_ENDED_OK 1
+#define STATUS_ENDED_ERROR 2
+
+#include <SDL2/SDL.h>
+
+class App {
+
+protected:
+	int status;
+
+public:
+	App() :
+		status(STATUS_RUNNING) {
+	}
+	;
+
+	virtual ~App() {
+	}
+	;
+
+	/**
+	 * Metodo para inicializar la app
+	 */
+	virtual bool initialize()=0;
+	/**
+	 * Metodo para manejar los eventos de sdl
+	 */
+	virtual void handle_event(SDL_Event& event)=0;
+	/**
+	 * Metodo para ejecutar durante el loop
+	 * (actualizacion de estados de la aplicacion)
+	 */
+	virtual void loop()=0;
+	/**
+	 * Metodo para ejecutar durante la etapa de
+	 * dibujado en pantalla
+	 */
+	virtual void render()=0;
+	/**
+	 * Metodo para limpiar el contenido de la app
+	 */
+	virtual void cleanup()=0;
+	/**
+	 * Obtiene el estado actual:
+	 * -STATUS_RUNNING
+	 * -STATUS_ENDED_OK
+	 * -STATUS_ENDED_ERROR
+	 */
+	int get_app_status() {
+		return status;
+	}
+	;
+
+};
+
+#endif /* APP_H_ */
---
+++ Level_Editor/editor_app.cpp
@@ -0,0 +1,112 @@
+/**
+ clientApp.cpp
+
+ Copyright 2013 Gaston Martinez Gaston.martinez.90@gmail.com
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+
+#include "editor_app.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_ttf.h>
+
+using std::vector;
+
+Editor_App::Editor_App() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	TTF_Init();
+	running = false;
+	apps = vector<App*>();
+
+	actual_app = 0;
+}
+
+Editor_App::~Editor_App() {
+    apps.clear();
+	SDL_Quit();
+}
+
+void Editor_App::change_app() {
+	if (apps[actual_app]->get_app_status() != STATUS_RUNNING) {
+		if (apps[actual_app]->get_app_status() != STATUS_ENDED_OK) {
+			running = false;
+			return;
+		}
+		switch (actual_app) {
+
+		case 2: //Tablero
+			apps[actual_app]->cleanup();
+			actual_app--;
+			apps[actual_app]->initialize();
+			return;
+
+		default:
+			apps[actual_app]->cleanup();
+			actual_app++;
+			apps[actual_app]->initialize();
+			return;
+		}
+	}
+}
+
+int Editor_App::run() {
+	if (initialize() == false) {
+		return 1;
+	}
+	SDL_Event Event;
+
+	while (running) {
+		change_app();
+		while (SDL_PollEvent(&Event)) {
+			handle_event(Event);
+		}
+		loop();
+		render();
+		SDL_Delay(17); //60fps aprox;
+	}
+
+	cleanup();
+	return 0;
+}
+
+bool Editor_App::initialize() {
+	running = true;
+	apps[0]->initialize();
+	return true;
+}
+
+void Editor_App::handle_event(SDL_Event& event) {
+	apps[actual_app]->handle_event(event);
+	if (event.type == SDL_QUIT) {
+		running = false;
+	}
+	return;
+}
+
+void Editor_App::loop() {
+	apps[actual_app]->loop();
+	return;
+}
+
+void Editor_App::render() {
+	apps[actual_app]->render();
+	return;
+}
+
+void Editor_App::cleanup() {
+	apps[actual_app]->cleanup();
+}
