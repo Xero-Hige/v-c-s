40bc73dd1891474f04e1790d2294b9c8005f1917
bruno290@gmail.com <bruno290@gmail.com@355a992d-334e-248d-a552-79595acbc279>
2013-12-02 05:53:54 +0000

    Makefile del server actualizado

--- trunk/Server/game_manager/game_manager.h
+++ trunk/Server/game_manager/game_manager.h
@@ -60,7 +60,6 @@ private:
     GameMessageBuilder msg_builder;
 
 public:
-    GameManager(Room* room);
     GameManager(Room* room, LevelReader* level_reader);
     void configure();
     bool addPlayer(std::string user_id);
